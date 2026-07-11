48b886da5c40a262bb0fc76d5c37f29879ee86ec
unknown <maksimssandykdorz4@gmail.com>
2026-07-11 17:41:17 +0800

    файлики

---
+++ api/auth/register.ts
@@ -0,0 +1,46 @@
+import { prisma } from '../../lib/prisma'
+import bcrypt from 'bcrypt'
+import { generateToken } from '../../utils/generateToken'
+
+const register = async (req: any, res: any) => {
+    const { name, email, password } = req.body;
+
+    // Check if user exists
+    const userExists = await prisma.user.findUnique({
+        where: {email: email}
+    });
+
+    if (userExists) {
+        return res.status(400).json({error: "User already exists with this email" })
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create User
+    const user = await prisma.user.create({
+        data: {
+            name, 
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    // Generate JWT Token
+    const token = generateToken(user.id, res)
+
+    res.status(201).json({
+        status: "success",
+        data: {
+            user: {
+                id: user.id,
+                name: name,
+                email: email
+            },
+            token
+        }
+    })
+}
+
+export default register
